e6f2cb1feab1028ea4334c9cc31455d8ce90efbd
ddannyll <daniel.nguyen.s173@gmail.com>
2022-05-24 18:36:16 +1000

    feat: implement show books function

--- books.js
+++ books.js
@@ -1,3 +1,5 @@
+const booksContainer = document.querySelector('#books-container') 
+
 let myLibrary = []
 
 function Book(title, author, pages, read) {
@@ -12,14 +14,52 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
-    let title = prompt('title?')
-    let author = prompt('author?')
-    let pages = +prompt('pages?')
-    let read = Boolean(prompt('read?'))
-    
+function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read)
     myLibrary.push(book)
 }
 
-addBookToLibrary()
+function createBookElement(title, author, pages, read) {
+    const bookCard = document.createElement('div')
+    bookCard.className = 'book-card'
+    
+    const titleElem = document.createElement('h3')
+    titleElem.innerText = title
+    bookCard.appendChild(titleElem)
+    
+    const authorElem = document.createElement('p')
+    authorElem.innerText = author
+    bookCard.appendChild(authorElem)
+
+    const pagesElem = document.createElement('p')
+    pagesElem.innerText = pages + 'pages'
+    bookCard.appendChild(pagesElem)
+
+    const readElem = document.createElement('button') 
+    read ? readElem.innerText = 'Unread' : readElem.innerText = 'Finish reading'
+    bookCard.appendChild(readElem)
+    
+    const removeBook = document.createElement('button')
+    removeBook.innerText = 'Remove'
+    bookCard.appendChild(removeBook)
+
+    return bookCard
+}
+
+function showLibrary() {
+    for (let book of myLibrary) {
+        const bookCard = createBookElement(book.name, book.author, book.pages, book.read)
+        booksContainer.appendChild(bookCard)
+    }
+}
+
+
+addBookToLibrary('title', 'author', 3, false)
+
+addBookToLibrary('title', 'author2', 99, false)
+
+addBookToLibrary('johns book', 'john', 99, true)
+
+showLibrary()
+
+

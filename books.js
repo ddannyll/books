bd49475d3216031e6fe997212213347f73418e53
ddannyll <daniel.nguyen.s173@gmail.com>
2022-05-24 17:47:20 +1000

    create html css, create addBookToLibrary JS func

--- books.js
+++ books.js
@@ -1,20 +1,25 @@
+let myLibrary = []
+
 function Book(title, author, pages, read) {
     this.name = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = () => {
-        str = `${this.title} by ${this.author}, ${this.pages} pages,\ `
+    this.info = function() {
+        str = `${this.name} by ${this.author}, ${this.pages} pages,\ `
         this.read ? str += 'has been read' : str += 'not read yet'
         return str
     }
 }
 
+function addBookToLibrary() {
+    let title = prompt('title?')
+    let author = prompt('author?')
+    let pages = +prompt('pages?')
+    let read = Boolean(prompt('read?'))
+    
+    let book = new Book(title, author, pages, read)
+    myLibrary.push(book)
+}
 
-let theHobbit = new Book('The Hobbit', 'J.K.R Tolkein', 295, false)
-console.log(theHobbit.info())
-theHobbit.author = 'ME'
-console.log(theHobbit.info())
-console.log(theHobbit.author)
-console.log(theHobbit.constructor)
-console.log(theHobbit.constructor.prototype)
+addBookToLibrary()
